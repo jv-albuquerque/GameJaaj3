0b5eec96740b42feb84d16920ca7593881266a9b
SamirRodrigues <emaildesamir@gmail.com>
2019-07-17 20:35:11 -0300

    Samir Assets
    Scripts and Prefabs made by Samir

---
+++ GameJaaj3/Assets/Samir/Scripts/IA_Combat_Ally.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IA_Combat_Ally : MonoBehaviour
+{
+    public float hp = 100f;
+    public float atkDamage = 25f;
+
+
+
+    //Enemy interation
+
+    private float timeBtwAttack;
+    public float startTimeBtwAttack = 1f;
+
+    public Transform attackPos; // Posição do ataque
+    public LayerMask whatIsEnemy; // O que é inimigo (Quem eu vou atacar)
+    public float attackRange = 2f; // Alcance do ataque
+    public bool vivo = true; // Variável que verifica se o personagem está vivo ou não
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Attack()
+    { 
+        if (timeBtwAttack <= 0) // Se o tempo entre um ataque e outro for menor ou igual a 0, então vocÊ pode atacar
+        {
+            Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemy); // Cria um trigger para verificar quantos inimigos estão na área de contato e coloca todos eles dentro de um vetor Collider
+
+            if (enemiesToDamage.Length > 0) // Se a quantidade de inimigos dentro da área for maior que 0
+            {
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                   // enemiesToDamage[i].GetComponent<Enemy>().Kill(); // Mata todos os inimigos que estão na área
+
+                }
+            }
+        timeBtwAttack = startTimeBtwAttack;
+        }            
+        else
+        {
+            timeBtwAttack -= Time.deltaTime;
+        }         
+        
+    }
+
+    void OnDrawGizmosSelected() // Cria um Gizmo na tela apra que seja possivel ver o a área do colisor mesmo quando ele estiver desativado
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+}
---
+++ GameJaaj3/Assets/Samir/Scripts/IA_Enemy.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IA_Enemy : MonoBehaviour
+{
+    //Movimentação
+    public Vector3 StartPosition;
+    public Vector3 EndPosition;
+
+    public int Point = 0;
+    public GameObject[] PointPosition;
+
+    private float distacePerFrame = 0f;
+    public float movePerFrame = 2f;
+       
+    // Quebra de rota
+    public bool isFighting = false;
+
+
+
+    void Start()
+    {
+        StartPosition = PointPosition[3].transform.position;
+        EndPosition = PointPosition[4].transform.position;
+    }
+
+    void Update()
+    {
+        if (isFighting == false)
+        {
+
+            distacePerFrame += movePerFrame / 100;
+            transform.position = Vector3.Lerp(StartPosition, EndPosition, distacePerFrame);
+
+            if (transform.position == EndPosition)
+            {
+                for (int i = Point; i < 4; i++)
+                {
+                    StartPosition = PointPosition[i].transform.position;
+                    EndPosition = PointPosition[i + 1].transform.position;
+
+                    distacePerFrame = 0; // Zera ao chegar no novo ponto
+
+                    Point += 1;
+                    
+                    if (i == 3)
+                    {
+                        Point = 0;
+                    }
+                    break;
+                }
+            }
+        } 
+    }
+    
+    void OnTriggerEnter(Collider Ally)
+    {
+        Debug.Log("!");
+        isFighting = true;
+    }
+    
+}

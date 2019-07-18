7e671afb3f91c2533b0ce1bfe30e13269b8567e1
Samir_Rodrigues <samitec@ufrn.edu.br>
2019-07-18 04:30:12 -0300

    Changes in AIs
    Drafts of logic behind AI

--- GameJaaj3/Assets/_Samir/Scripts/IA_Combat_Ally.cs
+++ GameJaaj3/Assets/_Samir/Scripts/IA_Combat_Ally.cs
@@ -7,12 +7,12 @@ public class IA_Combat_Ally : MonoBehaviour
     public float hp = 100f;
     public float atkDamage = 25f;
 
+    public bool isFighting = false;
+
 
 
     //Enemy interation
-
-    private float timeBtwAttack;
-    public float startTimeBtwAttack = 1f;
+    public bool checkArea = false;
 
     public Transform attackPos; // Posição do ataque
     public LayerMask whatIsEnemy; // O que é inimigo (Quem eu vou atacar)
@@ -23,30 +23,40 @@ public class IA_Combat_Ally : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isFighting == true)
+        {
+            Attack();
+        }
+        else
+        {
+            Patrol();
+        }
     }
 
     void Attack()
-    { 
-        if (timeBtwAttack <= 0) // Se o tempo entre um ataque e outro for menor ou igual a 0, então vocÊ pode atacar
-        {
-            Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemy); // Cria um trigger para verificar quantos inimigos estão na área de contato e coloca todos eles dentro de um vetor Collider
-
-            if (enemiesToDamage.Length > 0) // Se a quantidade de inimigos dentro da área for maior que 0
-            {
-                for (int i = 0; i < enemiesToDamage.Length; i++)
-                {
-                   // enemiesToDamage[i].GetComponent<Enemy>().Kill(); // Mata todos os inimigos que estão na área
-
-                }
-            }
-        timeBtwAttack = startTimeBtwAttack;
-        }            
-        else
-        {
-            timeBtwAttack -= Time.deltaTime;
-        }         
+    {
+        Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemy); // Cria um trigger para verificar quantos inimigos estão na área de contato e coloca todos eles dentro de um vetor Collider
         
+        if (enemiesToDamage.Length > 0) // Se a quantidade de inimigos dentro da área for maior que 0
+        {
+            for (int i = 0; i < enemiesToDamage.Length; i++)
+            {
+                 /*
+                 while (enemy.hp > 0)
+                 {
+                    enemy.hp -= atkDamage;
+                    cd(1);
+                 }
+                 */
+
+            }
+        }
+        
+    }
+
+    void Patrol()
+    {
+        // Quando não tiver inimigos, entrar em modo patrulha
     }
 
     void OnDrawGizmosSelected() // Cria um Gizmo na tela apra que seja possivel ver o a área do colisor mesmo quando ele estiver desativado

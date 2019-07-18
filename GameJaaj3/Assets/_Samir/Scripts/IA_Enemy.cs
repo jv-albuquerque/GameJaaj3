af9772386389d8e6086bee79c760e9d1a7b199ec
SamirRodrigues <emaildesamir@gmail.com>
2019-07-18 16:55:15 -0300

    IA_Enemy moviment edition

--- GameJaaj3/Assets/_Samir/Scripts/IA_Enemy.cs
+++ GameJaaj3/Assets/_Samir/Scripts/IA_Enemy.cs
@@ -5,15 +5,14 @@ using UnityEngine;
 public class IA_Enemy : MonoBehaviour
 {
     //Movimentação
-    public Vector3 StartPosition;
-    public Vector3 EndPosition;
+    
+    public Vector2 EndPosition;
 
     public int Point = 0;
-    public GameObject[] PointPosition;
+    public Transform PointPosition;
     private Vector2 target;
 
-    private float distacePerFrame = 0f;
-    public float movePerFrame = 2f;
+    private float distacePerFrame = 5f;    
        
     // Quebra de rota
     public bool isFighting = false;
@@ -22,44 +21,28 @@ public class IA_Enemy : MonoBehaviour
 
     void Start()
     {
-        StartPosition = PointPosition[3].transform.position;
-        EndPosition = PointPosition[4].transform.position;        
+        //Debug.Log(target);
+        EndPosition = target;        
     }
 
     void Update()
     {
         if (isFighting == false)
         {
-
-            distacePerFrame += movePerFrame / 100;
-            transform.position = Vector3.Lerp(StartPosition, EndPosition, distacePerFrame);
-
-            if (transform.position == EndPosition)
-            {
-                for (int i = Point; i < 4; i++)
-                {
-                    StartPosition = PointPosition[i].transform.position;
-                    EndPosition = PointPosition[i + 1].transform.position;
-
-                    distacePerFrame = 0; // Zera ao chegar no novo ponto
-
-                    Point += 1;
-                    
-                    if (i == 3)
-                    {
-                        Point = 0;
-                    }
-                    break;
-                }
-            }
+            Debug.Log(target);
+            transform.position = Vector2.MoveTowards(transform.position, EndPosition, distacePerFrame * Time.deltaTime);                
+           
         } 
     }
-    
-    void OnTriggerEnter(Collider Ally)
+    /*
+    void OnTriggerEnter2D(Collider2D Ally)
     {
         Debug.Log("!");
         isFighting = true;
     }
+     */
+
+    
 
 
     public Vector2 SetTarget
---
+++ GameJaaj3/Assets/_Samir/Scripts/TargetDefiner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetDefiner : MonoBehaviour
+{
+    [SerializeField]    private Transform [] nextTarget;
+    
+
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        Debug.Log("!");
+        if(coll.CompareTag("Enemy"))
+        {
+            int idx = Random.Range(0, nextTarget.Length-1);   
+            coll.GetComponent<IA_Enemy>().SetTarget = nextTarget[idx].position;
+        }
+    }
+}
